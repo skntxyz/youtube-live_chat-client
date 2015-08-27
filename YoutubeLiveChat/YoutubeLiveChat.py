87f70695b6a5d6a08a1313fcbc7dd56064cca5ee
0SkyneT <o0Sky.Net0@gmail.com>
2015-08-27 18:05:29 +0200

    comments

--- YoutubeLiveChat/YoutubeLiveChat.py
+++ YoutubeLiveChat/YoutubeLiveChat.py
@@ -1,22 +1,28 @@
-#gittest
-import time
 import os
-from bs4 import BeautifulSoup
-import selenium.webdriver as webdriver
+import sys
+import time
 import collections
+# beautifulsoup
+from bs4 import BeautifulSoup
+# selenium
+import selenium.webdriver as webdriver
 import selenium.webdriver.support.ui as ui
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import sys
+
+# unicode support
 import codecs
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 sys.stderr = codecs.getwriter('utf8')(sys.stderr)
 
-
+# init webdriver
 browser = webdriver.Firefox()
 browser.get(raw_input("enter url to live stream:"))
 
-ui.WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "comment-text")))
+# wait for the chat to load
+ui.WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.CLASS_NAME, "comment-text")))
+
+# get the element with all comments
 allcomments = browser.find_element_by_id("all-comments")
 
 # comments
@@ -30,29 +36,18 @@ while True:
     soup = BeautifulSoup(src)
 
     for li in soup.find_all("li"):
+		# get the username
         user = li.find_next("span", {"class":"author"})
         user = user.find_next("a").text
         user = unicode(user).lstrip().replace('\n','')
         usertext = "".join(user)
 
+		# get the commenttext
         comment = li.find("div", {"class":"comment-text"})
         comment = unicode(comment.text).lstrip().replace('\n','')
         commenttext = "".join(comment)
         
+		# if they dont exist already, print and add them
         if (usertext + ": " + commenttext) not in comments:
             comments.append(usertext + ": " + commenttext)
             print usertext + ": " + commenttext
-
-
-#while True:
-#    os.system(['clear','cls'][os.name == 'nt'])
-#    resp = urllib2.urlopen("http://www.youtube.com/user/AratorYT/live")
-#    src = resp.read()
-
-#    soup = BeautifulSoup(src)
-#    comments = soup.find_all("div", {"class":"comment-text"})
-#    print comments
-#    for comment in comments:
-#        print comment
-#    time.sleep(2)
-    
